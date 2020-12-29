bcc81d072f4291b0e546a272e54fecaa01540a80
dhouha karoui <dhouhakaroui@hotmail.com>
2020-12-29 19:54:00 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Button } from 'react-bootstrap';
 import React from 'react';
 import './App.css';
 import img from './image.jpeg'
@@ -15,35 +16,43 @@ class App extends React.Component {
       time:0
     }
   }
+  componentDidMount(){
+    setInterval(()=>{(this.state.show) ? this.setState({time:this.state.time+1}):this.setState({time:0})},1000)
+  };
   toggleShow=()=>this.setState({show:!this.state.show})          
   render(){
     return(
-      <div className="App">       
-        <button className="btn" onClick={this.toggleShow}>Click Me!</button>
-        <div style={{width:250,marginLeft:10}}>time since the component has mounted (second): {this.state.time}</div> 
+      <div className="App">  
+        <div>   
+          <button className="btn" onClick={this.toggleShow}>Click Me!</button>
+          <div style={{width:250,marginLeft:10}}>
+            time since the component has mounted (second): {this.state.time}
+          </div> 
+        </div>  
         {(this.state.show)?
         <div className="box"> 
-          <div className="row" >
-            <img src={this.state.person.imgSrc}  alt="phot" style={{width:300, height:400,border:"white 1px solid",borderRadius:"10px"}}/>
+          <div className="line">
+            <img src={this.state.person.imgSrc} alt="phot" className="img" />
+            <div className="parti">
+              <div className="row">
+                <h1>Full Name:</h1>
+                <div>{this.state.person.fullName}</div>
+              </div>
+              <div className="row">
+                <h1>Profession:</h1>
+                <div>{this.state.person.profession}</div>
+              </div>
+              <Button  onClick={()=>alert(this.state.person.fullName)}>
+              alert name of profile </Button>
+            </div> 
           </div>
-          <div className="row">
-            <h1>Full Name:</h1>
-            <div>{this.state.person.fullName}</div>
-          </div>
-          <div className="row">
-            <h1>Profession:</h1>
-            <div>{this.state.person.profession}</div>
-          </div> 
-          <div className="row">
+          <div className="row">    
             <h1>Bio:</h1>
             <div>{this.state.person.bio}</div>
-          </div>        
+          </div>       
         </div> : null }
       </div>
     );      
   }
-  componentDidMount(){
-    setInterval(()=>{(this.state.show) ? this.setState({time:this.state.time+1}):this.setState({time:0})},1000)
-  };
 }
 export default App;
